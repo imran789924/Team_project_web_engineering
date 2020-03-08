a87aed52db31f1400adff7443022d0756abade11
Imran <imran789924@gmail.com>
2020-03-09 00:07:38 +0600

    Front-end on progress

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
+
+
 <body>
 	<div class="container">
-		<h1>My First Bootstrap Page</h1>
-		<p>This is some text.</p>
+		<h1>Team portfolio and workspace</h1>
+		<p>Our team work is presented here...</p>
 	</div>
 
 	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -47,6 +50,98 @@
 	    </form>
 	  </div>
 	</nav>
+
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/Anik1.jpg" alt="Anik Das" width="1100" height="500">
+      <div>
+      	<h3>Anik Das</h3>
+      	<p>Machine Learning Engineer</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/Araf1.jpg" alt="Araf" width="1100" height="500">
+      <div>
+      	<h3>Shahriar Tasjid Araf</h3>
+      	<p>Machine Learning Engineer</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/Imran1.jpg" alt="New York" width="1100" height="500">
+      <div>
+      	<h3>Ashraful Alam</h3>
+      	<p>Machine Learning Learner</p>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12">
+				<img src="images/imran.jpg" class="img-fluid imransec">
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<h2 class="display-4">Ashraful Alam</h2>
+				<p class="py-3">I am imran. student of diu. ldfjldkf j ldjf dlfj ldj dklfj dkljl dkls fjldkfj lddklsjf kldsjf lkjldfj ldfj dfkdjjdflkdjklfjdklfjdklfjdklfjdklsfjdklsfjdlf ld fkjdhfkjdj klfjkldjklsfjdkl .</p>
+				<a href="about.php" class="btn btn-success">Detailed Portfolio</a>
+			</div>
+		</div><br>
+
+		<div >
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12">
+				<img src="images/anik.jpg" class="img-fluid imransec">
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<h2 class="display-4">Anik Das</h2>
+				<p class="py-3">I am imran. student of diu. ldfjldkf j ldjf dlfj ldj dklfj dkljl dkls 
+				fjldkfj lddklsjf kldsjf lkjldfj ldfj.</p>
+				<a href="about.php" class="btn btn-success">Detailed Portfolio</a>
+			</div>
+			
+		</div><br>
+
+		<div >
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-12">
+				<img src="images/araf.jpg" class="img-fluid imransec">
+			</div>
+			<div class="col-lg-6 col-md-6 col-12">
+				<h2 class="display-4">Shahriar Tasjid</h2>
+				<p class="py-3">I am imran. student of diu. ldfjldkf j ldjf dlfj ldj dklfj dkljl dkls fjldkfj lddklsjf kldsjf lkjldfj ldfj.</p>
+				<a href="about.php" class="btn btn-success">Detailed Portfolio</a>
+			</div>
+			
+		</div>
+</section>
+
 
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
